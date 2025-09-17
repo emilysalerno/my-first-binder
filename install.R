7a4058add85bf1dc396de956cdbe5ec0cbe5bdbf
emilysalerno <esalerno@unc.edu>
2025-09-17 17:27:19 -0400

    Update install.R

--- install.R
+++ install.R
@@ -2,6 +2,6 @@ options(repos = c(CRAN = "https://cran.r-project.org"))
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
-install.packages("shiny"), dependencies = TRUE)
+install.packages("shiny", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
